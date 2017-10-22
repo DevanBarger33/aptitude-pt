bb5e542e8767f9ea47c89aa4b7ead7a4d1e9c63b
Devan Barger <DevanBarger33@gmail.com>
2017-10-22 15:49:59 -0500

    forgot to add the HomeComponent class name, istead I had AboutComponent for the class name.

--- aptitude-app/src/app/components/home/home.component.ts
+++ aptitude-app/src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./home.component.css']
 })
 
-export class AboutComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
   constructor() { }
 
